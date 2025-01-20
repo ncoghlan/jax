d7c9b0ec5ed5b261b8f5d131af9495fd5b4aeee6
Tom Hennigan <tomhennigan@google.com>
2025-01-20 02:19:31 -0800

    Add an option to simplify `keystr` output and use a custom separator.
    Currently `keystr` just calls `str` on the key entries, leading to quite
    verbose output. For example:
    
        >>> params = {'foo': {'bar': {'baz': 1, 'bat': [2, 3]}}}
        ... for path, _ in jax.tree_util.tree_leaves_with_path(params):
        ...   print(jax.tree_util.keystr(path))
        ['foo']['bar']['bat'][0]
        ['foo']['bar']['bat'][1]
        ['foo']['bar']['baz']
    
    This change allows for a new "simple" format where the string representation
    of key entries are further simplified. Additionally we allow a custom
    separator since it is very common to use `/` (for example to separate module
    and parameter names):
    
        ... for path, _ in jax.tree_util.tree_leaves_with_path(params):
        ...   print(jax.tree_util.keystr(path, simple=True, separator='/'))
        foo/bar/bat/0
        foo/bar/bat/1
        foo/bar/baz
    ```
    
    PiperOrigin-RevId: 717455550

--- jax/_src/tree_util.py
+++ jax/_src/tree_util.py
@@ -722,22 +722,47 @@ FlattenedIndexKey: Any = pytree.FlattenedIndexKey  # type: ignore
 
 
 @export
-def keystr(keys: KeyPath):
+def keystr(keys: KeyPath, *, simple: bool = False, separator: str = '') -> str:
   """Helper to pretty-print a tuple of keys.
 
   Args:
     keys: A tuple of ``KeyEntry`` or any class that can be converted to string.
+    simple: If True, use a simplified string representation for keys.
+    separator: The separator to use to join string representations of the keys.
 
   Returns:
     A string that joins all string representations of the keys.
 
   Examples:
     >>> import jax
-    >>> keys = (0, 1, 'a', 'b')
-    >>> jax.tree_util.keystr(keys)
-    '01ab'
+    >>> params = {'foo': {'bar': {'baz': 1, 'bat': [2, 3]}}}
+    >>> for path, _ in jax.tree_util.tree_leaves_with_path(params):
+    ...   print(jax.tree_util.keystr(path))
+    ['foo']['bar']['bat'][0]
+    ['foo']['bar']['bat'][1]
+    ['foo']['bar']['baz']
+    >>> for path, _ in jax.tree_util.tree_leaves_with_path(params):
+    ...   print(jax.tree_util.keystr(path, simple=True, separator='/'))
+    foo/bar/bat/0
+    foo/bar/bat/1
+    foo/bar/baz
   """
-  return ''.join(map(str, keys))
+  str_fn = _simple_keystr if simple else str
+  return separator.join(map(str_fn, keys))
+
+
+def _simple_keystr(key: KeyEntry) -> str:
+  match key:
+    case SequenceKey(idx=idx):
+      return str(idx)
+    case DictKey(key=key):
+      return key
+    case GetAttrKey(name=name):
+      return name
+    case FlattenedIndexKey(key=idx_key):
+      return str(idx_key)
+    case _:
+      return str(key)
 
 
 # TODO(ivyzheng): remove this after another jaxlib release.
